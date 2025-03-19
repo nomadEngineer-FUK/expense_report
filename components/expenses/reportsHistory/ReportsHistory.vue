cbdce26360895bbf7e07259bd0477a6769bfdb04
nomadEngineer-FUK <s.hiraoka08@gmail.com>
2025-03-20 08:59:52 +0900

    申請一覧のレイアウト修正

--- components/expenses/reportsHistory/ReportsHistory.vue
+++ components/expenses/reportsHistory/ReportsHistory.vue
@@ -3,12 +3,17 @@ import { ref, onMounted } from 'vue';
 import { DISPLAYED_COLUMNS } from '~/types/types';
 import type { ExpenseReportType } from '~/types/types';
 import {
+    FilterType,
+    SortType,
+    filterOptions,
+    sortOptions
+} from '~/types/types';
+import {
     fetchExpenseReports,
     filterExpenseReports,
     sortExpenseReports
 } from '~/useCases/fetchExpenseReports';
 import { COLUMN_LABEL_MAP } from '~/constants';
-
 import DropdownSelect from '~/components/commonTools/DropdownSelect.vue';
 
 // カラム名を日本語へ変換
@@ -17,28 +22,8 @@ const mappedColumns = (col: string) => COLUMN_LABEL_MAP[col] || col;
 // 表示するカラムリスト
 const columnNames = ref(DISPLAYED_COLUMNS);
 
-// 承認状況のフィルター
-enum FilterType {
-    All = "all",
-    Approved = "approved",
-    Unapproved = "unapproved",
-};
-const filterOptions = [
-    { label: "すべて", value: FilterType.All },
-    { label: "承認済", value: FilterType.Approved },
-    { label: "未承認", value: FilterType.Unapproved },
-];
+// フィルター対象のカラム
 const filterType = ref<FilterType>(filterOptions[0].value);
-
-// ソート
-enum SortType {
-    Asc = "asc",
-    Desc = "desc",
-};
-const sortOptions = [
-    { label: "昇順", value: SortType.Asc },
-    { label: "降順", value: SortType.Desc },
-]
 
 // ソート対象のカラム
 const sortKey = ref<keyof ExpenseReportType>("id");
@@ -77,54 +62,59 @@ const getApprovalStatusClass = (approval: boolean) => {
     <div class="history-container">
         <h2 class="history-title">経費レポート履歴</h2>
 
-        <!-- 承認状況のプルダウン -->
-        <div class="filter-container">
-            <DropdownSelect label="承認状況" v-model="filterType" :options="filterOptions" />
-        </div>
+        <div class="history-content">
+            
+            <!-- 承認状況のプルダウン -->
+            <div class="dropdown-wrapper">
+                
+                <!-- フィルター用のプルダウン -->
+                <DropdownSelect label="承認状況" v-model="filterType" :options="filterOptions" />
+                
+                <!-- ソート用のプルダウン -->
+                <DropdownSelect label="ソート対象" v-model="sortKey" :options="sortKeyOptions" />
+                <DropdownSelect label="並び替え" v-model="sortOrder" :options="sortOptions" />
+            </div>
+            
+            <div class="table-wrapper">
+                <table>
+                    <thead>
+                        <tr>
+                            <th v-for="col in columnNames" :key="col" :class="`col-${col}`">
+                                {{ mappedColumns(col) }}
+                            </th>
+                            <th class="col-receipt">領収証</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- フィルターの結果がゼロ件の場合 -->
+                        <tr v-if="processedReports.length === 0">
+                            <td :colspan="columnNames.length + 1" class="no-data-message">
+                                該当の条件を満たすデータはありません
+                            </td>
+                        </tr>
 
-        <!-- ソート用のプルダウン -->
-        <DropdownSelect label="ソート対象" v-model="sortKey" :options="sortKeyOptions" />
-        <DropdownSelect label="並び替え" v-model="sortOrder" :options="sortOptions" />
-
-        <div class="table-wrapper">
-            <table>
-                <thead>
-                    <tr>
-                        <th v-for="col in columnNames" :key="col" :class="`col-${col}`">
-                            {{ mappedColumns(col) }}
-                        </th>
-                        <th class="col-receipt">領収証</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- フィルターの結果がゼロ件の場合 -->
-                    <tr v-if="processedReports.length === 0">
-                        <td :colspan="columnNames.length + 1" class="no-data-message">
-                            該当の条件を満たすデータはありません
-                        </td>
-                    </tr>
-
-                    <tr v-for="report in processedReports" :key="report.id">
-                        <td v-for="col in columnNames" :key="col" :class="`col-${col}`">
-                            <template v-if="col === 'approval'">
-                                <span :class="getApprovalStatusClass(report.approval)">
-                                    {{ getApprovalStatusText(report.approval) }}
-                                </span>
-                            </template>
-                            <template v-else-if="col === 'amount'">
-                                {{ report[col]?.toLocaleString() }} <!-- カンマ付きにする -->
-                            </template>
-                            <template v-else-if="col === 'create_date'">
-                                {{ report.formattedDate }} <!-- 日付のフォーマットに変更 -->
-                            </template>
-                            <template v-else>
-                                {{ report[col] }}
-                            </template>
-                        </td>
-                        <td class="col-receipt">link</td>
-                    </tr>
-                </tbody>
-            </table>
+                        <tr v-for="report in processedReports" :key="report.id">
+                            <td v-for="col in columnNames" :key="col" :class="`col-${col}`">
+                                <template v-if="col === 'approval'">
+                                    <span :class="getApprovalStatusClass(report.approval)">
+                                        {{ getApprovalStatusText(report.approval) }}
+                                    </span>
+                                </template>
+                                <template v-else-if="col === 'amount'">
+                                    {{ report[col]?.toLocaleString() }} <!-- カンマ付きにする -->
+                                </template>
+                                <template v-else-if="col === 'create_date'">
+                                    {{ report.formattedDate }} <!-- 日付のフォーマットに変更 -->
+                                </template>
+                                <template v-else>
+                                    {{ report[col] }}
+                                </template>
+                            </td>
+                            <td class="col-receipt">link</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
@@ -134,35 +124,53 @@ const getApprovalStatusClass = (approval: boolean) => {
     padding: 2rem;
     position: relative;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
 }
 
 .history-title {
     position: fixed;
     top: 0;
-    left: 0;
-    width: 100%;
+    left: 12%;
+    width: calc(100% - 12%);
     height: 50px;
     background: white;
-    text-align: center;
+    /* text-align: center; */
     border-bottom: 1px solid #ddd;
-    padding: 1rem 0 3rem;
-    z-index: 50;
+    padding: 2rem 0;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
-.filter-container {
-    margin-top: 60px;
+.history-content {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 20px;
+    margin-top: 50px;
 }
-.table-wrapper {
-    width: 90%;
-    margin: 30px auto 0;
-    overflow-y: auto;
-    max-height: calc(100vh - 100px);
+.dropdown-wrapper {
+    width: 180px;
+    flex-shrink: 0;
+    margin-top: 20px;
 }
 
+/* テーブル */
+.table-wrapper {
+    width: 90%;
+    margin: 20px auto 0;
+    overflow-y: auto;
+    max-height: calc(100vh - 150px);
+    flex-grow: 1;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 10px;
+}
 table {
     width: 100%;
     border-collapse: collapse;
 }
-
 thead {
     position: sticky;
     top: 0;
@@ -171,7 +179,6 @@ thead {
     border-bottom: 2px solid #ddd;
 
 }
-
 /* スクロール時の視認性を向上 */
 thead::after {
     content: "";
@@ -183,24 +190,20 @@ thead::after {
     height: 2px;
     background: #ddd;
 }
-
 th,
 td {
     border: 1px solid #ddd;
     padding: 8px 12px;
     text-align: center;
 }
-
 th {
     background-color: #f4f4f4;
     font-weight: bold;
 }
-
 tr:hover {
     background-color: rgba(255, 245, 157, 0.3) !important;
     transition: 0.3s;
 }
-
 tr:nth-child(even) {
     background-color: #fafafa;
 }
@@ -216,15 +219,13 @@ tr:nth-child(even) {
     width: 80px;
 }
 
-/* 承認済みのスタイル */
+/* 承認済み・未承認 */
 .status-approved {
     color: #2f5d3a;
     background-color: #d4edda;
     padding: 4px 8px;
     border-radius: 4px;
 }
-
-/* 未承認のスタイル */
 .status-unapproved {
     color: #6c2f2f;
     background-color: #f8d7da;
@@ -233,14 +234,58 @@ tr:nth-child(even) {
 }
 
 /* レスポンシブ対応 */
-@media (max-width: 600px) {
+@media (max-width: 1023px) {
+    .history-container {
+        overflow: hidden;
+    }
+    
+    .history-title {
+        left: 0;
+        width: 100%;
+        font-size: 1.2rem;
+        padding: 1rem 0;
+        flex-direction: column;
+    }
+
+    .history-content {
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+    }
+
+    .dropdown-wrapper {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .table-wrapper {
+        width: 100%;
+        max-height: calc(100vh - 350px);
+        overflow-x: auto;
+        overflow-y: auto;
+        padding-bottom: 20px;
+        margin-top: 5px;
+        display: block;
+        white-space: nowrap;
+    }
+
     table {
-        font-size: 14px;
+        width: 800px;
     }
 
     th,
     td {
-        padding: 6px 10px;
+        padding: 5px 8px;
+        font-size: 12px;
+    }
+
+    .col-approval,
+    .col-purchase_date,
+    .col-create_date,
+    .col-amount {
+        width: auto;
     }
 }
 </style>
