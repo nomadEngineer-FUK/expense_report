b67eb489cf88a70b41b6516d198fdf93f74ee0f0
nomadEngineer-FUK <s.hiraoka08@gmail.com>
2025-03-30 14:45:53 +0900

    不要コード削除

--- components/expenses/reportForm/ReportSectionHeader.vue
+++ components/expenses/reportForm/ReportSectionHeader.vue
@@ -1,31 +1,3 @@
-<!-- <script setup lang="ts">
-const props = defineProps<{
-    label: string
-}>();
-</script>
-
-<template>
-    <h3 class="report-title">{{ props.label }}</h3>
-</template>
-
-<style scoped>
-.report-title {
-    font-weight: bold;
-    font-size: larger;
-    padding: 0.8rem;
-    margin: 3rem auto 2rem;
-    border-top-right-radius: 24px;
-    border-bottom-left-radius: 24px;
-    border-top-left-radius: 8px;
-    border-bottom-right-radius: 8px;
-    width: 95%;
-    border: 2px solid #000080;
-    color: #000080;
-    background-color: #c3dcf4e5;
-    background: linear-gradient(to right, #f0f8ff, #c3dcf4e5);
-}
-</style> -->
-
 <template>
     <div class="report-title-wrapper">
         <h3 class="report-title">{{ props.label }}</h3>
